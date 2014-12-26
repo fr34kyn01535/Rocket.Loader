b5f63d7f3543ecbba51b5b74ce8bdd4a42d54db8
fr34kyn01535 <fr34kyn01535@bam.yt>
2014-12-26 01:08:15 +0100

    crel

--- RocketAPI/CommandReload.cs
+++ RocketAPI/CommandReload.cs
@@ -19,7 +19,7 @@ namespace Rocket.RocketAPI
 
         protected override void execute(SteamPlayerID m, string s)
         {
-            Bootstrap.RocketAPI.LoadPlugins();
+            Bootstrap.RocketAPI = new Core();
             Logger.Log("Reloaded Rocket");
             ChatManager.say("Reloaded Rocket");
         }
