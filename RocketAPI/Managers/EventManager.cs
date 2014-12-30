9c0711865382d197ff321ac167a70971966c7025
fr34kyn01535 <fr34kyn01535@bam.yt>
2014-12-31 00:36:06 +0100

    EventManager

---
+++ RocketAPI/Managers/EventManager.cs
@@ -0,0 +1,40 @@
+﻿using Rocket.RocketAPI.Commands;
+using SDG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rocket.RocketAPI
+{
+    public class EventManager
+    {
+        public event Steam.ServerConnected PlayerConnected;
+        public event Steam.ServerDisconnected PlayerDisconnected;
+
+        private Dictionary<string,DateTime> players = new Dictionary<string,DateTime>();
+
+        public EventManager()
+        {
+            SDG.Steam.serverConnected += onPlayerConnected;
+            SDG.Steam.serverDisconnected += onPlayerDisconnected;
+        }
+
+        private void onPlayerDisconnected(Steamworks.CSteamID id)
+        {
+            if (PlayerDisconnected != null)
+            PlayerDisconnected(id);
+        }
+
+        private void onPlayerConnected(Steamworks.CSteamID id)
+        {
+            if (PlayerConnected != null)
+            PlayerConnected(id);
+        }
+
+        internal void Reload()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
