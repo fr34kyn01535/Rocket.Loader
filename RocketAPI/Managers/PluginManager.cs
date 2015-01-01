655bc90c82efd9e1a28a91baf438710e0f745e65
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-01-01 21:13:35 +0100

    Added Libraries folder

--- RocketAPI/Managers/PluginManager.cs
+++ RocketAPI/Managers/PluginManager.cs
@@ -15,23 +15,64 @@ namespace Rocket.RocketAPI.Managers
         
         internal PluginManager()
         {
+            if (!Directory.Exists(Bootstrap.HomeFolder + "Plugins/")) Directory.CreateDirectory(Bootstrap.HomeFolder + "Plugins/");
+            if (!Directory.Exists(Bootstrap.HomeFolder + "Plugins/Libraries/")) Directory.CreateDirectory(Bootstrap.HomeFolder + "Plugins/Libraries/");
+
+            loadLibraries();
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(resolveHandler);
             loadPlugins();
+        }
+
+        private void loadLibraries()
+        {
+            FileInfo[] libraries = new DirectoryInfo(Bootstrap.HomeFolder + "Plugins/Libraries/").GetFiles("*.dll");
+            foreach (FileInfo library in libraries)
+            {
+                try
+                {
+                    AssemblyName name = AssemblyName.GetAssemblyName(library.FullName);
+                    assemblyNameToFileMapping.Add(name.FullName, library.FullName);
+                    //Logger.Log(library.FullName + " is " + name.FullName);
+                }
+                catch { } 
+            }
+        }
+
+        private Dictionary<string, string> assemblyNameToFileMapping = new Dictionary<string, string>();
+
+        private Assembly resolveHandler(object sender, ResolveEventArgs args)
+        {
+            //Logger.Log("Trying to get " + args.Name);
+            string file;
+            if (assemblyNameToFileMapping.TryGetValue(args.Name, out file))
+            {
+                return Assembly.LoadFrom(file);
+            }
+            return null;
         }
 
         private void loadPlugins()
         {
-            if (!Directory.Exists(Bootstrap.HomeFolder + "Plugins/")) Directory.CreateDirectory(Bootstrap.HomeFolder + "Plugins/");
-
             List<Type> pluginTypes = new List<Type>();
             try
-            {
-                FileInfo[] libraries = new DirectoryInfo(Bootstrap.HomeFolder + "Plugins/").GetFiles("*.dll");
+            {    
+                FileInfo[] pluginsLibraries = new DirectoryInfo(Bootstrap.HomeFolder + "Plugins/").GetFiles("*.dll");
 
-                foreach (FileInfo library in libraries)
+                foreach (FileInfo library in pluginsLibraries)
                 {
                     Assembly assembly = Assembly.LoadFile(library.FullName);
-                    Type[] types = assembly.GetTypes();
-                    foreach (Type type in types)
+                    Type[] types;
+
+                    try
+                    {
+                        types = assembly.GetTypes();
+                    }
+                    catch (ReflectionTypeLoadException e)
+                    {
+                        types = e.Types;
+                    }
+
+                    foreach (Type type in types.Where(t => t != null))
                     {
                         if (type.IsInterface || type.IsAbstract)
                         {
