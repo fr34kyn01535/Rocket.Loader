3a03cddbfaccca95c81d156b7a9d5a96f60598a0
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-01-04 10:21:53 +0100

    Update to Rocket 3.4.9.1

---
+++ RocketAPI/Components/RocketPlayerComponent.cs
@@ -0,0 +1,57 @@
+﻿using SDG;
+using UnityEngine;
+
+namespace Rocket
+{
+    public class RocketPlayerComponent : MonoBehaviour
+    {
+        private Player _player;
+        public Player player
+        {
+            get { return _player; }
+            set { _player = value; }
+        }
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(transform.gameObject);
+            player = gameObject.GetComponent<Player>();
+            player.PlayerLife.OnDamaged += onDamaged;
+            player.PlayerLife.OnUpdateBleeding += onUpdateBleeding;
+            player.PlayerLife.OnUpdateBroken += onUpdateBroken;
+            player.PlayerLife.OnUpdateFoot += onUpdateFoot;
+            player.PlayerLife.OnUpdateFreezing += onUpdateFreezing;
+            player.PlayerLife.OnUpdateHealth += onUpdateHealth;
+            player.PlayerLife.OnUpdateLife += onUpdateLife;
+            player.PlayerLife.OnUpdateOxygen += onUpdateOxygen;
+            player.PlayerLife.OnUpdateStamina += onUpdateStamina;
+            player.PlayerLife.OnUpdateVirus += onUpdateVirus;
+            player.PlayerLife.OnUpdateVision += onUpdateVision;
+            player.PlayerLife.OnUpdateWater += onUpdateWater;
+        }
+
+        protected virtual void onUpdateBleeding(bool J) { }
+
+        protected virtual void onUpdateBroken(bool D) { }
+
+        protected virtual void onUpdateFoot(byte R) { }
+
+        protected virtual void onUpdateFreezing(bool h) { }
+
+        protected virtual void onUpdateHealth(byte Y) { }
+
+        protected virtual void onUpdateLife(bool C) { }
+
+        protected virtual void onUpdateOxygen(byte t) { }
+
+        protected virtual void onUpdateStamina(byte D) { }
+
+        protected virtual void onUpdateVirus(byte E) { }
+
+        protected virtual void onUpdateVision(bool o) { }
+
+        protected virtual void onUpdateWater(byte A) { }
+
+        protected virtual void onDamaged(byte P) { }
+    }
+}
---
+++ RocketAPI/Static Helper/Commands.cs
@@ -0,0 +1,25 @@
+﻿using SDG;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Rocket
+{
+    public class Commands : MonoBehaviour
+    {
+        public static void RegisterCommand(Command command)
+        {
+            foreach (Command ccommand in Commander.commandList)
+            if (ccommand.commandName.ToLower().Equals(command.commandName.ToLower()))
+            {
+                Logger.Log("Command already registered: " + command.GetType().FullName);
+                return;
+            }
+
+            List<Command> commandList = Commander.commandList.ToList();
+            commandList.Add(command);
+            Commander.commandList = commandList.ToArray();
+        }
+    }
+
+}
