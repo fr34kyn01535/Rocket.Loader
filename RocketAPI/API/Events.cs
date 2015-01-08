52c236b2985bf8609a1e15f0286b5af8f7e83108
Robin Egede <minedcraft@hotmail.com>
2015-01-08 22:20:31 +0100

    Added some documentation

--- RocketAPI/API/Events.cs
+++ RocketAPI/API/Events.cs
@@ -55,6 +55,11 @@ namespace Rocket.RocketAPI
 
 
         #region PlayerLife
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="player">The victim player</param>
+        /// <param name="bleeding">Flag for bleeding</param>
         public delegate void PlayerUpdateBleeding(Player player, bool bleeding);
         public static event PlayerUpdateBleeding OnPlayerUpdateBleeding;
         private void onUpdateBleeding(bool J)
@@ -62,6 +67,11 @@ namespace Rocket.RocketAPI
             if (OnPlayerUpdateBleeding != null) OnPlayerUpdateBleeding(player, J);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player">The victim player</param>
+        /// <param name="broken">Flag for broken legs</param>
         public delegate void PlayerUpdateBroken(Player player, bool broken);
         public static event PlayerUpdateBroken OnPlayerUpdateBroken;
         private void onUpdateBroken(bool D)
@@ -69,6 +79,11 @@ namespace Rocket.RocketAPI
             if (OnPlayerUpdateBroken != null) OnPlayerUpdateBroken(player, D);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player">The victim player</param>
+        /// <param name="freezing"></param>
         public delegate void PlayerUpdateFreezing(Player player, bool freezing);
         public static event PlayerUpdateFreezing OnPlayerUpdateFreezing;
         private void onUpdateFreezing(bool h)
@@ -76,6 +91,11 @@ namespace Rocket.RocketAPI
             if (OnPlayerUpdateFreezing != null) OnPlayerUpdateFreezing(player, h);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player">The victim player</param>
+        /// <param name="life"></param>
         public delegate void PlayerUpdateLife(Player player, bool life);
         public static event PlayerUpdateLife OnPlayerUpdateLife;
         private void onUpdateLife(bool C)
@@ -83,6 +103,11 @@ namespace Rocket.RocketAPI
             if (OnPlayerUpdateLife != null) OnPlayerUpdateLife(player, C);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player">The victim player</param>
+        /// <param name="vision"></param>
         public delegate void PlayerUpdateVision(Player player, bool vision);
         public static event PlayerUpdateVision OnPlayerUpdateVision;
         private void onUpdateVision(bool o)
@@ -90,6 +115,11 @@ namespace Rocket.RocketAPI
             if (OnPlayerUpdateVision != null) OnPlayerUpdateVision(player, o);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player">The victim player</param>
+        /// <param name="foot"></param>
         public delegate void PlayerUpdateFoot(Player player, byte foot);
         public static event PlayerUpdateFoot OnPlayerUpdateFoot;
         private void onUpdateFoot(byte R)
@@ -97,6 +127,11 @@ namespace Rocket.RocketAPI
             if (OnPlayerUpdateFoot != null) OnPlayerUpdateFoot(player, R);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player">The victim player</param>
+        /// <param name="health">New player health value</param>
         public delegate void PlayerUpdateHealth(Player player, byte health);
         public static event PlayerUpdateHealth OnPlayerUpdateHealth;
         private void onUpdateHealth(byte Y)
@@ -104,7 +139,11 @@ namespace Rocket.RocketAPI
             if (OnPlayerUpdateHealth != null) OnPlayerUpdateHealth(player, Y);
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player">The victim player</param>
+        /// <param name="oxygen">Flag for wether or not the player is above water</param>
         public delegate void PlayerUpdateOxygen(Player player, byte oxygen);
         public static event PlayerUpdateOxygen OnPlayerUpdateOxygen;
         private void onUpdateOxygen(byte t)
@@ -112,6 +151,11 @@ namespace Rocket.RocketAPI
             if (OnPlayerUpdateOxygen != null) OnPlayerUpdateOxygen(player, t);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player">The victim player</param>
+        /// <param name="stamina">New player stamina value</param>
         public delegate void PlayerUpdateStamina(Player player, byte stamina);
         public static event PlayerUpdateStamina OnPlayerUpdateStamina;
         private void onUpdateStamina(byte D)
@@ -119,7 +163,11 @@ namespace Rocket.RocketAPI
             if (OnPlayerUpdateStamina != null) OnPlayerUpdateStamina(player, D);
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player">The victim player</param>
+        /// <param name="virus">New player radiation/infection value</param>
         public delegate void PlayerUpdateVirus(Player player, byte virus);
         public static event PlayerUpdateVirus OnPlayerUpdateVirus;
         private void onUpdateVirus(byte E)
@@ -127,13 +175,22 @@ namespace Rocket.RocketAPI
             if (OnPlayerUpdateVirus != null) OnPlayerUpdateVirus(player, E);
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player">The victim player</param>
+        /// <param name="water">New player water value</param>
         public delegate void PlayerUpdateWater(Player player, byte water);
         public static event PlayerUpdateWater OnPlayerUpdateWater;
         private void onUpdateWater(byte A) {
             if (OnPlayerUpdateWater != null) OnPlayerUpdateWater(player, A);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player">The victim player</param>
+        /// <param name="damage">Damage to apply to the player</param>
         public delegate void PlayerDamaged(Player player, byte damage);
         public static event PlayerDamaged OnPlayerDamaged;
         private void onDamaged(byte P)
@@ -177,6 +234,10 @@ namespace Rocket.RocketAPI
 
         #region Steam
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player">The disconnecting player</param>
         public delegate void PlayerDisconnected(Player player);
         public static event PlayerDisconnected OnPlayerDisconnected;
         private static void onPlayerDisconnected(CSteamID r)
@@ -184,6 +245,10 @@ namespace Rocket.RocketAPI
             if (OnPlayerDisconnected != null) OnPlayerDisconnected(PlayerTool.getPlayer(r));
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player">The connecting player</param>
         public delegate void PlayerConnected(Player player);
         public static event PlayerConnected OnPlayerConnected;
 
