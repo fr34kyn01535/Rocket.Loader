efbed81c8e14c015170c3c6e5d878a3f1ba3030f
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-01-19 00:23:12 +0100

    added Multithreading helper

--- RocketAPI/Properties/AssemblyInfo.cs
+++ RocketAPI/Properties/AssemblyInfo.cs
@@ -8,4 +8,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8870d132-f877-4fbd-9e73-49c8b1af8b3f")]
-[assembly: AssemblyVersion("2.0.5.1")]
+[assembly: AssemblyVersion("2.0.6.0")]
