e0e6ca32a4e8ebc4a18a1743c299027293ca8bbd
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-01-24 01:38:07 +0100

    Console

--- RocketAPI/Managers/RocketChatManager.cs
+++ RocketAPI/Managers/RocketChatManager.cs
@@ -16,12 +16,13 @@ namespace Rocket.RocketAPI
 
         public static void Say(string message, EChatMode chatmode = EChatMode.GLOBAL)
         {
+            Logger.Log("Broadcast: "+message);
             ChatManager.Instance.SteamChannel.send("tellChat", ESteamCall.OTHERS, ESteamPacket.UPDATE_UDP_BUFFER, new object[] { CSteamID.Nil, (byte)chatmode, message });
         }
 
         public static void Say(CSteamID CSteamID, string message, EChatMode chatmode = EChatMode.SAY)
         {
-            if (CSteamID != null) 
+            if(CSteamID.ToString() != "0")
             ChatManager.Instance.SteamChannel.send("tellChat", CSteamID, ESteamPacket.UPDATE_UDP_BUFFER, new object[] { CSteamID.Nil, (byte)chatmode, message });
         }
     }
