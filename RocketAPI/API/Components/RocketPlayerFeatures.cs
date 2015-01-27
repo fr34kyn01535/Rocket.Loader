dd295f840d9e2ee81177e07090184d3337abe521
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-01-27 23:00:50 +0100

    .

---
+++ RocketAPI/API/Components/RocketPlayerFeatures.cs
@@ -0,0 +1,77 @@
+﻿using Rocket.RocketAPI.Components;
+using SDG;
+using Steamworks;
+using System;
+using UnityEngine;
+
+namespace Rocket.RocketAPI
+{
+    public class RocketPlayerFeatures : RocketPlayerComponent
+    {
+        RocketPlayer p = null;
+        Player pl = null;
+        private bool godMode = false;
+        internal bool GodMode{
+            set{
+                if(value){
+                    RocketEvents.OnPlayerUpdateHealth += events_OnPlayerUpdateHealth;
+                    RocketEvents.OnPlayerUpdateWater += events_OnPlayerUpdateWater;
+                    RocketEvents.OnPlayerUpdateFood += events_OnPlayerUpdateFood;
+                    RocketEvents.OnPlayerUpdateVirus += events_OnPlayerUpdateVirus;
+                }else{
+                    RocketEvents.OnPlayerUpdateHealth -= events_OnPlayerUpdateHealth;
+                    RocketEvents.OnPlayerUpdateWater -= events_OnPlayerUpdateWater;
+                    RocketEvents.OnPlayerUpdateFood -= events_OnPlayerUpdateFood;
+                    RocketEvents.OnPlayerUpdateVirus -= events_OnPlayerUpdateVirus;
+                }
+                godMode = value;
+            }
+            get{
+                return godMode;
+            }
+        }
+
+        private void Start(){
+
+            p = gameObject.transform.GetComponent<RocketPlayer>();
+            pl = gameObject.transform.GetComponent<Player>();
+
+            if (godMode)
+            {
+                RocketEvents.OnPlayerUpdateHealth += events_OnPlayerUpdateHealth;
+                RocketEvents.OnPlayerUpdateWater += events_OnPlayerUpdateWater;
+                RocketEvents.OnPlayerUpdateFood += events_OnPlayerUpdateFood;
+                RocketEvents.OnPlayerUpdateVirus += events_OnPlayerUpdateVirus;
+                p.Heal(100);
+                p.Infection = 0;
+                p.Hunger = 0;
+                p.Thirst = 0;
+            }
+            if (!RocketPermissionManager.CheckWhitelisted(pl.SteamChannel.SteamPlayer.SteamPlayerID.CSteamID)) return;
+
+        }
+
+
+        
+        void events_OnPlayerUpdateVirus(Player player, byte virus)
+        {
+            if (virus != 100) p.Infection = 0;
+        }
+
+        void events_OnPlayerUpdateFood(Player player, byte food)
+        {
+            if (food != 100) p.Hunger = 0;
+        }
+
+        void events_OnPlayerUpdateWater(Player player, byte water)
+        {
+            if (water != 100) p.Thirst = 0;
+        }
+
+        void events_OnPlayerUpdateHealth(Player player, byte health)
+        {
+            if (health != 100) p.Heal(100);
+        }
+
+    }
+}
---
+++ RocketAPI/RocketSettings.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Rocket
+{
+    public class RocketSettings
+    {
+        public static string HomeFolder;
+        public static bool EnableRcon = false;
+        public static string RconPassword = "changeme";
+        public static int RconPort = 0;
+
+        [XmlElement(ElementName="EnableRcon")]
+        public bool enableRcon {
+            get {
+                return EnableRcon;
+            }
+            set {
+                EnableRcon = value; ;
+            }
+        }
+
+        [XmlElement(ElementName = "RconPassword")]
+        public string rconPassword
+        {
+            get
+            {
+                return RconPassword;
+            }
+            set
+            {
+                RconPassword = value; ;
+            }
+        }
+
+        [XmlElement(ElementName = "RconPort")]
+        public int rconPort
+        {
+            get
+            {
+                return RconPort;
+            }
+            set
+            {
+                RconPort = value; ;
+            }
+        }
+
+        internal void Load() { 
+            
+            XmlSerializer serializer = new XmlSerializer(typeof(RocketSettings));
+            string configFile = Path.Combine(HomeFolder,"Rocket.config");
+            if(File.Exists(configFile)){
+                RocketSettings s = (RocketSettings)serializer.Deserialize(new StreamReader(configFile));
+                enableRcon = s.enableRcon;
+                if(s.rconPassword !=null) rconPassword = s.rconPassword;
+                rconPort = s.rconPort;
+            }else{
+                serializer.Serialize(new StreamWriter(configFile), new RocketSettings());
+            }
+        }
+    }
+}
