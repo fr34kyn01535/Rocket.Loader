3193d6fed1bc145554c1809ee4daee946ff602c9
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-01-28 23:11:24 +0100

    !

---
+++ RocketAPI/API/Components/Events/RocketEvents.cs
@@ -0,0 +1,23 @@
+﻿using Rocket.RocketAPI.Components;
+
+namespace Rocket.RocketAPI
+{
+    public partial class RocketEvents : RocketPlayerComponent
+    {
+        private new void Awake()
+        {
+            base.Awake();
+
+            #region RocketPlayerLifeEvents
+                PlayerInstance.PlayerLife.OnUpdateStamina += onUpdateStamina;
+            #endregion RocketPlayerLifeEvents
+
+            #region RocketPlayerEvents
+                PlayerInstance.Inventory.OnInventoryAdded += onInventoryAdded;
+                PlayerInstance.Inventory.OnInventoryRemoved += onInventoryRemoved;
+                PlayerInstance.Inventory.OnInventoryResized += onInventoryResized;
+                PlayerInstance.Inventory.OnInventoryUpdated += onInventoryUpdated;
+            #endregion RocketPlayerEvents
+        }
+    }
+}
