fe11e39992707177618b9ff9b24269fd7ee8bd42
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-02-01 08:33:26 +0100

    ftps dat weird basterd

--- RocketAPI/RocketLauncher.cs
+++ RocketAPI/RocketLauncher.cs
@@ -16,7 +16,7 @@ namespace Rocket
 
         public static RocketSettings Settings;
 
-        public float updateInterval = 0.5F;
+        public float updateInterval = 0.005F;
 
         public static DateTime Started = DateTime.UtcNow;
 
@@ -24,11 +24,6 @@ namespace Rocket
         private float accum = 0;
         private int frames = 0;
         private float timeleft;
-
-        public static float FTPS = 0; 
-        private float faccum = 0;
-        private int fframes = 0;
-        private float ftimeleft; 
         
         public static void Launch()
         {
@@ -67,7 +62,6 @@ namespace Rocket
                 Logger.LogError("Error while loading Rocket: " + e.ToString());
             }
             timeleft = updateInterval;
-            ftimeleft = updateInterval;  
         }
         void Update()
         {
@@ -84,24 +78,6 @@ namespace Rocket
                 timeleft = updateInterval;
                 accum = 0.0F;
                 frames = 0;
-            }
-        }
-
-        void FixedUpdate()
-        {
-            ftimeleft -= Time.deltaTime;
-            faccum += Time.timeScale / Time.deltaTime;
-            ++fframes;
-
-            if (ftimeleft <= 0.0)
-            {
-                TPS = faccum / fframes;
-
-                int left = Console.CursorLeft;
-                int top = Console.CursorTop;
-                ftimeleft = updateInterval;
-                faccum = 0.0F;
-                fframes = 0;
             }
         }
     }
