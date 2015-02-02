0ee8919a5c5a5d87a9e847548a73bb525e24f615
Robin Egede <minedcraft@hotmail.com>
2015-02-02 23:47:14 +0100

    Debug logger logs to file

--- RocketAPI/API/ExternalLogger.cs
+++ RocketAPI/API/ExternalLogger.cs
@@ -13,40 +13,76 @@ namespace Rocket.RocketAPI
         public static void ExternalLog(object msg)
         {
 
+            if(msg != null)
+                processLog("[Info] " + msg.ToString());
+
         }
 
         public static void ExternalLog(object msg, object context)
         {
+
+            if (msg != null)
+                processLog("[Info] " + msg.ToString());
+
+            if (context != null)
+                processLog("[Info] [Context] " + context.ToString());
 
         }
 
         public static void ExternalLogError(object msg)
         {
 
+            if (msg != null)
+                processLog("[Error] " + msg.ToString());
+
         }
 
         public static void ExternalLogError(object msg, object context)
         {
+
+            if (msg != null)
+                processLog("[Error] " + msg.ToString());
+
+            if (context != null)
+                processLog("[Error] [Context] " + msg.ToString());
 
         }
 
         public static void ExternalLogException(Exception ex)
         {
 
+            if (ex != null)
+                processLog("[Exception] " + ex);
+
         }
 
         public static void ExternalLogException(Exception ex, object context)
         {
+
+            if (ex != null)
+                processLog("[Exception] " + ex);
+
+            if (context != null)
+                processLog("[Exception] [Context] " + context.ToString());
 
         }
 
         public static void ExternalLogWarning(object msg)
         {
 
+            if (msg != null)
+                processLog("[Warning] " + msg.ToString());
+
         }
 
         public static void ExternalLogWarning(object msg, object context)
         {
+
+            if (msg != null)
+                processLog("[Warning] " + msg.ToString());
+
+            if (context != null)
+                processLog("[Warning] [Context] " + context.ToString());
 
         }
 
