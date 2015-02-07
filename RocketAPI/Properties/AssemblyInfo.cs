33491b98b437bba35a3af50f8935a93d798361db
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-02-07 14:43:47 +0100

    Version build

--- RocketAPI/Properties/AssemblyInfo.cs
+++ RocketAPI/Properties/AssemblyInfo.cs
@@ -4,7 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RocketAPI")]
 [assembly: AssemblyCompany("Rocket")]
 [assembly: AssemblyProduct("RocketAPI")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: ComVisible(false)]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: Guid("8870d132-f877-4fbd-9e73-49c8b1af8b3f")]
-[assembly: AssemblyVersion("2.3.2.0")]
