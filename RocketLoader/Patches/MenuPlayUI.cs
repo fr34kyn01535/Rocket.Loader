84a787e9cf3670d8666dd2d34cd5a26334a0180d
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-03-02 11:16:58 +0100

    I wanna be the very best - gotta fix 'em all! (Fixed shutdown issue)

---
+++ RocketLoader/Patches/MenuPlayUI.cs
@@ -0,0 +1,15 @@
+﻿using Mono.Cecil;
+
+namespace Rocket.RocketLoader.Patches
+{
+    public class MenuPlayUI : Patch
+    {
+        private PatchHelper h = new PatchHelper("SDG.MenuPlayUI");
+
+        public void Apply()
+        {
+            MethodDefinition ctor = h.GetMethod(".ctor");
+            ctor.Body.Instructions.Clear();
+        }
+    }
+}
