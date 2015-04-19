63ad787dc71e6e3507d0373e98d2a777d2656cfa
fr34kyn01535 <ihi4kah>
2015-04-19 21:14:19 +0200

    Extended InventoryGroup

--- RocketAPI/Rocket/RocketAPI/Events/RocketPlayerInventoryEvents.cs
+++ RocketAPI/Rocket/RocketAPI/Events/RocketPlayerInventoryEvents.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 namespace Rocket.RocketAPI.Events
 {
-    public enum InventoryGroup { Primary = 0, Secondary = 1, Backpack = 3, Vest = 4, Shirt = 5, Pants = 6, Storage = 7 };
+    public enum InventoryGroup { Primary = 0, Secondary = 1, Hands = 2, Backpack = 3, Vest = 4, Shirt = 5, Pants = 6, Storage = 7 };
 
     public sealed partial class RocketPlayerEvents : RocketPlayerComponent
     {
