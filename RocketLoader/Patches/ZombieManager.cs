e7fd6e39381a51939db561562d4e3343cf3daaa3
fr34kyn01535 <ihi4kah>
2015-04-26 00:31:42 +0200

    Unlocked ZombieManager.ZombieRegion [] & ZombieRegion.List<Zombie>

---
+++ RocketLoader/Patches/ZombieManager.cs
@@ -0,0 +1,12 @@
+﻿namespace Rocket.RocketLoader.Patches
+{
+    public class ZombieManager : Patch
+    {
+        private PatchHelper h = new PatchHelper("SDG.ZombieManager");
+
+        public void Apply()
+        {
+            h.UnlockFieldByType("ZombieRegion[]", "ZombieRegions");
+        }
+    }
+}
---
+++ RocketLoader/Patches/ZombieRegion.cs
@@ -0,0 +1,12 @@
+﻿namespace Rocket.RocketLoader.Patches
+{
+    public class ZombieRegion : Patch
+    {
+        private PatchHelper h = new PatchHelper("SDG.ZombieRegion");
+
+        public void Apply()
+        {
+            h.UnlockFieldByType("List<Zombie>", "Zombies");
+        }
+    }
+}
