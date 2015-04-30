d5e407bc41021ce774d9a91ebf69d89645dd8ea5
fr34kyn01535 <ihi4kah>
2015-04-30 12:29:13 +0200

    Updated VersionInfo path

--- VersionInfo/Program.cs
+++ VersionInfo/Program.cs
@@ -14,7 +14,7 @@ namespace VersionInfo
         {
             if (args.Length != 3) Environment.Exit(1);
             WebClient wc = new WebClient();
-            wc.DownloadData(String.Format("http://api.rocket.foundation/jenkins/version.php?build={0}&buildid={1}&rocketversion={2}&unturnedversion={3}", args[1], args[2], getRocketVersion(), getUnturnedVersion()));
+            wc.DownloadData(String.Format("http://api.rocket.foundation/VersionInfo/report.php?build={0}&buildid={1}&rocketversion={2}&unturnedversion={3}", args[1], args[2], getRocketVersion(), getUnturnedVersion()));
         }
 
         private static string getRocketVersion()
