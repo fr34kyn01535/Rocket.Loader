99c952207e2d0844585c7e136e6a1908b902e567
fr34kyn01535 <ihi4kah>
2015-05-16 22:08:51 +0200

    ..

---
+++ Rocket.API/Rocket.API/IRocketImplementation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Rocket.API
+{
+    public interface IRocketImplementation
+    {
+        string InstanceName { get; }
+        string HomeFolder { get; }
+        string PluginsFolder { get; }
+        string LibrariesFolder { get; }
+        string LogsFolder { get; }
+        string ConfigurationFolder { get; }
+
+        Dictionary<string, string> Translation { get; }
+
+        ImplementationSettingsSection Configuration { get; }
+    }
+}
