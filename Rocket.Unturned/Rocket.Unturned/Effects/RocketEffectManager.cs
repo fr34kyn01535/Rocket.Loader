9ba7128eb6f54b1c7db58c6642db3dac20d8795a
fr34kyn01535 <ihi4kah>
2015-06-05 14:56:43 +0200

    Added player die EffectEvent (2)

--- Rocket.Unturned/Rocket.Unturned/Effects/RocketEffectManager.cs
+++ Rocket.Unturned/Rocket.Unturned/Effects/RocketEffectManager.cs
@@ -2,6 +2,7 @@
 using Rocket.Unturned.Logging;
 using Rocket.Unturned.Player;
 using SDG;
+using Steamworks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,33 +11,64 @@ using UnityEngine;
 
 namespace Rocket.Unturned
 {
+    public enum EffectEventTypes { Join = 1 , Die = 2};
+
     public class RocketEffectManager : MonoBehaviour
     {
         public void Start(){
             RocketServerEvents.OnPlayerConnected += (RocketPlayer player) =>
             {
-                foreach (ushort effect in getEffectsByEvent(EffectEvents.Join))
+                foreach (EffectEvent effect in getEffectsByEvent(EffectEventTypes.Join))
                 {
-                    player.TriggerEffect(effect);
+                    TriggerEffect(effect.EffectID, player, effect.Global);
+                }
+            };
+            RocketPlayerEvents.OnPlayerDeath += (RocketPlayer player, EDeathCause cause, ELimb limb, CSteamID murderer) => {
+                foreach (EffectEvent effect in getEffectsByEvent(EffectEventTypes.Die))
+                {
+                    TriggerEffect(effect.EffectID, player, effect.Global);
                 }
             };
         }
 
-        private ushort[] getEffectsByEvent(EffectEvents e)
-        {
-            return effects.Where(k => k.Value == e).Select(k => k.Key).ToArray();
+        public void TriggerEffect(ushort effectID,RocketPlayer player,bool global){
+            if(!global){
+                EffectManager.Instance.SteamChannel.send("tellEffectPoint", player.CSteamID, ESteamPacket.UPDATE_UDP_BUFFER, new object[] { effectID, player.Player.transform.position });
+            }else{
+                EffectManager.Instance.E.send("tellEffectPoint", ESteamCall.CLIENTS, player.Player.transform.position,1024, ESteamPacket.UPDATE_UDP_BUFFER, new object[] { effectID, player.Player.transform.position });
+            }
         }
 
-        public enum EffectEvents { Join = 1 };
+        private List<EffectEvent> getEffectsByEvent(EffectEventTypes e)
+        {
+            return effects.Where(k => k.Type == e).ToList();
+        }
 
-        private static Dictionary<ushort, EffectEvents> effects = new Dictionary<ushort, EffectEvents>();
+        private static List<EffectEvent> effects = new List<EffectEvent>();
 
         public static void RegisterRocketEffect(Bundle b, Data q, ushort k)
         {
             short s = q.readInt16("RocketEffect");
             if (s != 0){
-                effects.Add(k,(EffectEvents)s);
+                bool global = q.readBoolean("Global");
+
+                effects.Add(new EffectEvent((EffectEventTypes)s, k, global));
             }
         }
+    
+        private class EffectEvent
+        {
+            public EffectEvent(EffectEventTypes type, ushort effectID, bool global)
+            {
+                this.Type = type;
+                this.EffectID = effectID;
+                this.Global = global;
+            }
+            public EffectEventTypes Type;
+            public ushort EffectID;
+            public bool Global;
+        }
+
     }
+
 }
