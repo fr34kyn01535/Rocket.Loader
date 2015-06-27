cb878e22e99a169e7698007f36cfe056f6bb9cd9
fr34kyn01535 <ihi4kah>
2015-06-27 13:54:39 +0200

    Unlocking ItemManager to allow plugins to sweep items

---
+++ Rocket.Loader.Unturned/Patches/ItemManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Rocket.RocketLoader.Unturned.Patches
+{
+    [Class("SDG.Unturned.ItemManager")]
+    public class ItemManager : Patch
+    {
+        public override void Apply()
+        {
+            UnlockFieldByType("ItemRegion[,]", "ItemRegions");
+            UnlockFieldByType("ItemManager", "Instance");
+        }
+    }
+}
