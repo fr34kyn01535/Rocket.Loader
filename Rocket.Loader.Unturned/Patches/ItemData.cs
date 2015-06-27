7378654b4d827fd37449f3cd471fa5797ea58b36
fr34kyn01535 <ihi4kah>
2015-06-27 16:47:39 +0200

    Unlocked ItemData

---
+++ Rocket.Loader.Unturned/Patches/ItemData.cs
@@ -0,0 +1,14 @@
+﻿namespace Rocket.RocketLoader.Unturned.Patches
+{
+    [Class("SDG.Unturned.ItemData")]
+    public class ItemData : Patch
+    {
+        public override void Apply()
+        {
+            UnlockFieldByType("Item", "Item");
+            UnlockFieldByType("Vector3", "Position");
+            UnlockFieldByType(typeof(bool), "IsDropped");
+            UnlockFieldByType(typeof(float), "LastDropped");
+        }
+    }
+}
