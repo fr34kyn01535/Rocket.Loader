3826430141407542b671e5c213b2cf4c232c0743
fr34kyn01535 <ihi4kah>
2015-07-11 02:19:09 +0200

    .

--- Rocket.Unturned/Scripts/Linux/start.sh
+++ Rocket.Unturned/Scripts/Linux/start.sh
@@ -3,19 +3,32 @@
 # Syntax: start.sh <instance name>
 # Author: fr34kyn01535
 
-INSTANCE_NAME=$1
-UNTURNED_HOME="./unturned"
+# Before you start the Unturned 3 server on linux make sure the following packages are installed:
+#htop unzip # Utils                                                                           
+#screen Xorg xinit x11-common # Headless X server
+#libmono2.0-cil mono-runtime # Mono 2
+#libglu1-mesa libxcursor1 libxrandr2 libc6:i386 libgl1-mesa-glx:i386 libxcursor1:i386 libxrandr2:i386 # 32/64 bit prerequisites for unity3d
 
-ulimit -n 2048
-cd $UNTURNED_HOME
-if [ ! -f $UNTURNED_HOME/RocketLauncher.exe ]; then
-	if [ -f "RocketLauncher.exe" ]; then
-		mv RocketLauncher.exe $UNTURNED_HOME/RocketLauncher.exe
+INSTANCE_NAME=$1
+LAUNCHER="./unturned/RocketLauncher"
+
+if [ ! -f $LAUNCHER ]; then #Well, somebody can't configure bash scripts...
+	if [ -f ../../../install_304930.vdf ]; then 
+		LAUNCHER="../../../RocketLauncher"
+		if [ ! -f $LAUNCHER ]; then
+			if [ -f RocketLauncher ]; then
+				mv RocketLauncher $LAUNCHER
+			fi
+		fi
 	fi
 fi
 
-if [ -f $UNTURNED_HOME/RocketLauncher.exe ]; then
-	mono RocketLauncher.exe $UNTURNED_HOME
+ulimit -n 2048
+
+if [ -f $LAUNCHER ]; then
+	mono $LAUNCHER $UNTURNED_HOME
 else
 	echo "RocketLauncher not found"
 fi
+
+
