ceb22fe1041552733aed03121603e2a6aa069ecf
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-12-17 12:55:28 +0100

    Checking profile fields for null

--- Rocket.Core/Steam/Profile.cs
+++ Rocket.Core/Steam/Profile.cs
@@ -13,16 +13,16 @@ namespace Rocket.Core.Steam
         public string OnlineState { get; set; }
         public string StateMessage { get; set; }
         public string PrivacyState { get; set; }
-        public ushort VisibilityState { get; set; }
+        public ushort? VisibilityState { get; set; }
         public Uri AvatarIcon { get; set; }
         public Uri AvatarMedium { get; set; }
         public Uri AvatarFull { get; set; }
-        public bool IsVacBanned { get; set; }
+        public bool? IsVacBanned { get; set; }
         public string TradeBanState { get; set; }
-        public bool IsLimitedAccount { get; set; }
+        public bool? IsLimitedAccount { get; set; }
         public string CustomURL { get; set; }
-        public DateTime MemberSince { get; set; }
-        public double HoursPlayedLastTwoWeeks { get; set; }
+        public DateTime? MemberSince { get; set; }
+        public double? HoursPlayedLastTwoWeeks { get; set; }
         public string Headline { get; set; }
         public string Location { get; set; }
         public string RealName { get; set; }
@@ -69,24 +69,24 @@ namespace Rocket.Core.Steam
             XmlDocument doc = new XmlDocument();
             doc.LoadXml(new WebClient().DownloadString("http://steamcommunity.com/profiles/" + SteamID64 + "?xml=1"));
 
-            SteamID = doc["profile"]["steamID"].InnerText;
-            OnlineState = doc["profile"]["onlineState"].InnerText;
-            StateMessage = doc["profile"]["stateMessage"].InnerText;
-            PrivacyState = doc["profile"]["privacyState"].InnerText;
-            VisibilityState = ushort.Parse(doc["profile"]["visibilityState"].InnerText);
-            AvatarIcon = new Uri(doc["profile"]["avatarIcon"].InnerText);
-            AvatarMedium = new Uri(doc["profile"]["avatarMedium"].InnerText);
-            AvatarFull = new Uri(doc["profile"]["avatarFull"].InnerText);
-            IsVacBanned = doc["profile"]["vacBanned"].InnerText == "1";
-            TradeBanState = doc["profile"]["tradeBanState"].InnerText;
-            IsLimitedAccount = doc["profile"]["isLimitedAccount"].InnerText == "1";
-            CustomURL = doc["profile"]["customURL"].InnerText;
-            MemberSince = DateTime.Parse(doc["profile"]["memberSince"].InnerText.Replace("st", "").Replace("nd", "").Replace("rd", "").Replace("th", ""), new CultureInfo("en-US", false)); ;
-            HoursPlayedLastTwoWeeks = double.Parse(doc["profile"]["hoursPlayed2Wk"].InnerText);
-            Headline = doc["profile"]["headline"].InnerText;
-            Location = doc["profile"]["location"].InnerText;
-            RealName = doc["profile"]["realname"].InnerText;
-            Summary = doc["profile"]["summary"].InnerText;
+            SteamID = doc["profile"]["steamID"]?.InnerText;
+            OnlineState = doc["profile"]["onlineState"]?.InnerText;
+            StateMessage = doc["profile"]["stateMessage"]?.InnerText;
+            PrivacyState = doc["profile"]["privacyState"]?.InnerText;
+            VisibilityState = doc["profile"]["visibilityState"] != null ? (ushort?)ushort.Parse(doc["profile"]["visibilityState"].InnerText) : null;
+            AvatarIcon = doc["profile"]["avatarIcon"] != null ? new Uri(doc["profile"]["avatarIcon"].InnerText) : null;
+            AvatarMedium = doc["profile"]["avatarMedium"] != null ? new Uri(doc["profile"]["avatarMedium"].InnerText) : null;
+            AvatarFull = doc["profile"]["avatarFull"] != null ? new Uri(doc["profile"]["avatarFull"].InnerText) : null;
+            IsVacBanned = doc["profile"]["vacBanned"] == null ? (bool?)(doc["profile"]["vacBanned"].InnerText == "1") : null;
+            TradeBanState = doc["profile"]["tradeBanState"]?.InnerText;
+            IsLimitedAccount = doc["profile"]["isLimitedAccount"] == null ? (bool?)(doc["profile"]["isLimitedAccount"].InnerText == "1") : null;
+            CustomURL = doc["profile"]["customURL"]?.InnerText;
+            MemberSince = doc["profile"]["memberSince"] != null ? (DateTime?)DateTime.Parse(doc["profile"]["memberSince"].InnerText.Replace("st", "").Replace("nd", "").Replace("rd", "").Replace("th", ""), new CultureInfo("en-US", false)): null;
+            HoursPlayedLastTwoWeeks = doc["profile"]["hoursPlayed2Wk"] != null ? (double?)double.Parse(doc["profile"]["hoursPlayed2Wk"].InnerText) : null;
+            Headline = doc["profile"]["headline"]?.InnerText;
+            Location = doc["profile"]["location"]?.InnerText;
+            RealName = doc["profile"]["realname"]?.InnerText;
+            Summary = doc["profile"]["summary"]?.InnerText;
 
             MostPlayedGames = new List<MostPlayedGame>();
             foreach (XmlElement mostPlayedGame in doc["profile"]["mostPlayedGames"].ChildNodes)
